7d22582365907c7cf17e5882e6016f03b1ddc03f
bjohan <sa6bxq>
2019-03-18 22:44:54 +0100

    Did some cleaning

---
+++ src/py/SdrServer/fftSignalProcessor.py
@@ -0,0 +1,9 @@
+import signalProcessor
+import numpy as np
+def FftSignalProcessor(signalProcessor.SignalProcessor):
+    def __init__(self):
+        pass
+
+    def process(self, signalIn):
+                return np.fft.fftshift(np.fft.fft(s-np.mean(s)))
+
---
+++ src/py/SdrServer/signalProcessor.py
@@ -0,0 +1,6 @@
+class SignalProcessor:
+    def __init__(self):
+        pass
+
+    def process(self, signalIn):
+        return signalIn
