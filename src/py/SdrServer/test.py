44129cb3cfec94b85b6e3be632b8b75dd42f7715
bjohan <sa6bxq>
2019-04-09 08:12:46 +0200

    RX now works well enough, things are audible

--- src/py/SdrServer/test.py
+++ src/py/SdrServer/test.py
@@ -7,7 +7,7 @@ import dsp.fir
 import dsp.plot
 import dsp.db
 import dsp.limiter
-import dsp.decimate
+import dsp.resample
 import dsp.agc
 import dsp.fmDemod
 import dsp.sink
@@ -18,6 +18,7 @@ import dsp.fftPlot
 import dsp.status
 import dsp.tuner
 import dsp.squelch
+import dsp.acCouple
 
 
 d = soapySdrDevice.SoapySdrManager()
@@ -47,12 +48,12 @@ sndDev  = soundDevice.SoundDevice()
 
 print "Setting up processors"
 sdrsrc = dsp.source.Source(sdr, samples = 32768, fc = sdr.getFrequency()['rx'], rate = sdr.getSampleRate()['rx'])
-firdsp = dsp.fir.Fir(taps = 64, passBand=0.1);
+firdsp = dsp.fir.Fir(taps = 256, passBand=0.00001);
 fmdsp = dsp.fmDemod.FmDemod()
 agcdsp = dsp.agc.Agc(target=1, maxAmp = 10, rate =0.01)
 fftdsp = dsp.fft.Fft(samples=4096)
 dbdsp = dsp.db.Db()
-decdsp = dsp.decimate.Decimate(factor=91)
+decdsp = dsp.resample.Resample(outputRate=44000)
 limitdsp = dsp.limiter.Limiter(mi=-1, ma = 1)
 sndsink = dsp.sink.Sink(sndDev);
 plotdsp = dsp.plot.Plot("Demodulated fm");
@@ -60,11 +61,13 @@ plotfm = dsp.plot.Plot("decimated td");
 plotfft = dsp.fftPlot.FftPlot("Decimated spectrum", nmax = 512)
 plotfftfull = dsp.fftPlot.FftPlot("full spectrum", nmax = 512)
 plotfftfir = dsp.fftPlot.FftPlot("firfiltered full spectrum", nmax=512)
-squelch = dsp.squelch.Squelch(threshold = 0.4, hold = 0.00000)
+squelch = dsp.squelch.Squelch(threshold = 0.01, hold = 0.00000)
+ac = dsp.acCouple.AcCouple()
+ac2 = dsp.acCouple.AcCouple()
 
 statdsp = dsp.status.Status()
 tunedsp = dsp.tuner.Tuner(nco=-1500)
-dsp = dspPipeLine.DspPipeLine([ [sdrsrc], [plotfftfull], [firdsp],[plotfftfir], [decdsp],[tunedsp], [plotfft], [plotfm], [squelch], [fmdsp],[plotdsp], [agcdsp],[sndsink], [statdsp]])# [agcdsp], [sndsink], [fftdsp], [dbdsp, limitdsp], [plotdsp]])
+dsp = dspPipeLine.DspPipeLine([ [sdrsrc], [ac], [plotfftfull], [firdsp],[plotfftfir], [decdsp],[tunedsp], [plotfft], [plotfm], [squelch], [fmdsp],[ac2], [plotdsp], [agcdsp],[sndsink], [statdsp]])# [agcdsp], [sndsink], [fftdsp], [dbdsp, limitdsp], [plotdsp]])
 
 print "starting pipeline"
 dsp.start()
